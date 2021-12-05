e2cbf6303f06f6860e8d2373f5149495b99fb649
Jason Kai <tkai@uwo.ca>
2021-12-05 09:32:03 -0500

    refactor day01

--- day01/day01.py
+++ day01/day01.py
@@ -1,20 +1,11 @@
 import utils
 
 
-def compare_values(val1: int, val2: int):
-    return val2 > val1
+def count_larger(in_data: list[int]) -> int:
+    return sum(in_data[idx - 1] < in_data[idx] for idx in range(1, len(in_data)))
 
 
-def count_larger(in_data):
-    count = 0
-    for idx in range(1, len(in_data)):
-        if compare_values(in_data[idx - 1], in_data[idx]):
-            count += 1
-
-    return count
-
-
-def compute_window(in_data, window=3):
+def compute_window(in_data: list[int], window: int = 3):
     return [
         sum(in_data[idx : idx + window]) for idx in range(len(in_data) - window + 1)
     ]
