586309de4aa3eb9afe90197df002639b72d519e3
Jason Kai <tkai@uwo.ca>
2023-12-02 00:37:11 -0500

    Add day02 solutions

---
+++ day02/day02.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import re
+from unittest import TestCase
+
+
+class Day02:
+    def __init__(self, input: str):
+        self.total_cubes = {"red": 12, "green": 13, "blue": 14}
+        self.games = self.load_games(input)
+
+    def load_games(self, input: str) -> dict[int, dict[str, list[int]]]:
+        games: dict[int, dict[str, list[int]]] = {}
+        with open(input, "r", encoding="utf-8") as in_file:
+            for line in in_file:
+                game, record = line.split(": ")
+                game = int(game.split(" ")[-1])
+                games[game] = {
+                    colour: [
+                        int(val)
+                        for val in re.findall(rf"(\d+) {colour}", record)
+                    ]
+                    for colour in self.total_cubes.keys()
+                }
+
+            return games
+
+    def find_valid_games(self) -> list[int]:
+        valid_games = [
+            game for game in self.games.keys() if self.is_valid_game(game)
+        ]
+        return valid_games
+
+    def is_valid_game(self, game: int) -> bool:
+        return all(
+            max(self.games[game][colour]) <= self.total_cubes[colour]
+            for colour in self.total_cubes.keys()
+        )
+
+    def find_game_power(self) -> list[int]:
+        power = [
+            max(self.games[game]["red"])
+            * max(self.games[game]["blue"])
+            * max(self.games[game]["green"])
+            for game in self.games.keys()
+        ]
+        return power
+
+
+class TestMain(TestCase):
+    def test_part1(self):
+        test = Day02("./day02/test_input_part1.txt")
+        self.assertEqual(sum(test.find_valid_games()), 8)
+
+    def test_part2(self):
+        test = Day02("./day02/test_input_part2.txt")
+        self.assertEqual(sum(test.find_game_power()), 2286)
+
+
+if __name__ == "__main__":
+    solution = Day02("./day02/input.txt")
+    print(sum(solution.find_valid_games()))
+    print(sum(solution.find_game_power()))
