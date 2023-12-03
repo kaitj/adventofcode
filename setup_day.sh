697dd201413a7b85562a185a7d0643ba823f8205
Jason Kai <tkai@uwo.ca>
2023-12-03 16:21:23 -0500

    Update setup script

--- setup_day.sh
+++ setup_day.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # This is a small script to setup files for AOC day
+
 # Ensure at least 2 digits (e.g. 01, 02, 10, 11, etc.)
-day=$1
-while [[ ${#day} -lt 2 ]]; do
-  day="0${day}"
-done
+day=$(printf "%02d" $1)
 
 # Create folder
 day_prefix=Day${day}
@@ -12,21 +10,23 @@ mkdir -p $day_prefix
 
 # Create python file
 py_file=${day_prefix}/${day_prefix}.py
-echo "#!/usr/bin/env python" > ${py_file}
-echo "from pathlib import Path" >> ${py_file}
-echo "from unittest import TestCase" >> ${py_file}
-echo "" >> ${py_file}
-echo "class ${day_prefix}:" >> ${py_file}
-echo "" >> ${py_file}
-echo "class TestMain(TestCase):" >> ${py_file}
-echo "    def test_part1(self):" >> ${py_file}
-echo "        test = ${day_prefix}(f'{Path(__file__).parent}/test_input_part1.txt')" >> ${py_file}
-echo "" >> ${py_file}
-echo "    # def test_part2(self):" >> ${py_file}
-echo "    #     test = ${day_prefix}(f'{Path(__file__).parent}/test_input_part2.txt')" >> ${py_file}
-echo "" >> ${py_file}
-echo "# if __name__ == '__main__':" >> ${py_file}
-echo "#     solution = ${day_prefix}(f'{Path(__file__).parent}/input.txt')" >> ${py_file}
+cat > $py_file << EOF
+#!/usr/bin/env python
+from pathlib import Path
+from unittest import TestCase
+
+class ${day_prefix}:
+
+class TestMain(TestCase):
+    def test_part1(self):
+        test = ${day_prefix}(f"{Path(__file__).parent}/test_input_part1.txt")
+
+    # def test_part2(self):
+    #   test = ${day_prefix}(f"{Path(__file__).parent}/test_input_part2.txt")
+
+# __name__ == "__main__":
+#   solution = ${day_prefix}(f"{Path(__file__).parent}/input.txt")
+EOF
 
 # Create input files
 touch ${day_prefix}/input.txt
