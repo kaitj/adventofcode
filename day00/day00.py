6f12101d2c266bbfb8e71fec8ca79aeb198e7cbe
Jason Kai <tkai@uwo.ca>
2023-12-11 22:28:47 -0500

    Add template day (00)

---
+++ day00/day00.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+
+class Day00:
+    def __init__(self, input_path: str):
+        raise NotImplementedError()
+
+
+class TestMain:
+    def test_part1(self):
+        raise NotImplementedError()
+        test = Day00(f"{Path(__file__).parent}/test_input_part1.txt")
+
+    def test_part2(self):
+        raise NotImplementedError()
+        test = Day00(f"{Path(__file__).parent}/test_input_part2.txt")
+
+
+if __name__ == "__main__":
+    raise NotImplementedError()
+    solution = Day00(f"{Path(__file__).parent}/input.txt")
