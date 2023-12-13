17009424cbd192e25814f178e64f39fbe6855e07
Jason Kai <tkai@uwo.ca>
2023-12-13 16:43:03 -0500

    Add day 13, part 2
    Refactor reflection finding code to search by line instead of comparing
    pairs of lines.

--- day13/day13.py
+++ day13/day13.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from pathlib import Path
+from typing import Sequence
 
 from aoc.utils import day_parser
 
@@ -8,41 +9,50 @@ class Day13:
     def __init__(self, input_path: str):
         self.valleys = self.load_valleys(input_path)
 
+    def transpose(self, valley: Sequence[str]) -> list[str]:
+        return ["".join(line) for line in zip(*valley)]
+
+    def diff(self, line1: str, line2: str) -> int:
+        return sum(1 for ch1, ch2 in zip(line1, line2) if ch1 != ch2)
+
     def load_valleys(self, input_path: str) -> list[list[str]]:
         with Path(input_path).open(encoding="utf-8") as in_file:
-            contents = in_file.read()
+            contents = in_file.read().strip()
         valleys = contents.split("\n\n")
         valleys = [[*valley.split("\n")] for valley in valleys]
 
         return valleys
 
-    def find_mirror(self, valley: list[str]) -> int:
-        """
-        valley[idx] - right reflection
-        line (or idx - 1) = left reflection
-        """
-        mirror = False
-        for idx, line in enumerate(valley[:-1], start=1):
-            # Found a possible reflection
-            if valley[idx] == line:
-                # Check left half == right half
-                mirror = all(
-                    left == right
-                    for left, right in zip(valley[idx - 1 :: -1], valley[idx:])
-                )
+    def find_mirror(self, valley: list[str], no_smudges: int) -> int:
+        length = len(valley[0])
+        max_mirror_size = length // 2
 
-            if mirror:
-                return idx
+        for midx in reversed(range(1, max_mirror_size + 1)):
+            for shift in [0, length - 2 * midx]:
+                smudges = no_smudges
+                for row in valley:
+                    smudges -= self.diff(
+                        row[shift : shift + midx],
+                        row[shift + midx : shift + 2 * midx][::-1],
+                    )
 
-        return 0  # pyright: ignore
+                if smudges != 0:
+                    continue
 
-    def summarize(self) -> list[int]:
+                return shift + midx
+
+        return 0
+
+    def summarize(self, no_smudges: int = 0) -> list[int]:
         # Find vertical
-        vert = [self.find_mirror(list(zip(*valley))) for valley in self.valleys]
+        vert = [self.find_mirror(valley, no_smudges) for valley in self.valleys]
         # Find horizontal
-        horz = [self.find_mirror(valley) for valley in self.valleys]
+        horz = [
+            100 * self.find_mirror(self.transpose(valley), no_smudges)
+            for valley in self.valleys
+        ]
 
-        valleys_summary = [v_vert + 100 * v_horz for v_vert, v_horz in zip(vert, horz)]
+        valleys_summary = [v_vert + v_horz for v_vert, v_horz in zip(vert, horz)]
 
         return valleys_summary
 
@@ -50,13 +60,13 @@ class Day13:
 class TestMain:
     def test_part1(self) -> None:
         test = Day13(f"{Path(__file__).parent}/test_input_part1.txt")
-        assert test.find_mirror(list(zip(*test.valleys[0]))) == 5  # pyright: ignore
-        assert test.find_mirror(test.valleys[1]) == 4  # pyright: ignore
+        assert test.find_mirror(test.valleys[0], 0) == 5
+        assert test.find_mirror(test.transpose(test.valleys[1]), 0) == 4
         assert sum(test.summarize()) == 405
 
     def test_part2(self) -> None:
-        raise NotImplementedError()
-        # test = Day13(f"{Path(__file__).parent}/test_input_part2.txt")
+        test = Day13(f"{Path(__file__).parent}/test_input_part1.txt")
+        assert sum(test.summarize(no_smudges=1)) == 400
 
 
 def main():
@@ -66,7 +76,7 @@ def main():
     if args.part == 1:
         print(sum(solution.summarize()))
     elif args.part == 2:
-        raise NotImplementedError()
+        print(sum(solution.summarize(no_smudges=1)))
     else:
         raise ValueError("Not a valid part")
 
