bd2b2912afe7bb8707b9accabe5c9d9a2c6d03bd
Jason Kai <21226986+kaitj@users.noreply.github.com>
2024-12-08 13:39:47 -0500

    Add day 8

--- src/lib.rs
+++ src/lib.rs
@@ -72,3 +72,29 @@ impl Direction {
         }
     }
 }
+
+pub struct Matrix<T> {
+    pub cells: Vec<Vec<T>>,
+    pub cols: usize,
+    pub rows: usize,
+}
+
+impl<T: Copy> Matrix<T> {
+    pub fn get(&self, point: &Point) -> T {
+        self.cells[point.y as usize][point.x as usize]
+    }
+
+    pub fn point_inside(&self, point: &Point) -> bool {
+        point.x >= 0 && point.x < self.cols as isize && point.y >= 0 && point.y < self.rows as isize
+    }
+
+    pub fn neighbour(&self, point: &Point, direction: Direction) -> Option<Point> {
+        let neighbour = point.neighbour(direction);
+
+        if self.point_inside(&neighbour) {
+            Some(neighbour)
+        } else {
+            None
+        }
+    }
+}
